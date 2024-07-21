bac7781724a3e91e8484b1e707d86a1b75fe8ea3
devnydv <80939011+devnydv@users.noreply.github.com>
2024-07-21 08:51:27 +0530

    Update app.py
    Lite change

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def details():
     return render_template("details.html", data= details)
 
 
-if __name__== "__main__":
-    app.run(debug=True)
+#if __name__== "__main__":
+ #   app.run(debug=True)
