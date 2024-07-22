717026b8a5f7444ba8f944e95d42514fc81e79c9
devnydv <80939011+devnydv@users.noreply.github.com>
2024-07-23 01:25:35 +0530

    Update app.py
    Test

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 
 from flask import Flask, render_template
-from flipkartgrid import senddata
-#from flpkartp import detailsdata
+#from flipkartgrid import senddata
+from flpkartp import detailsdata
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    data = senddata()
-    return render_template("index.html", data= data)
-
+    #data = senddata()
+    #return render_template("index.html", data= data)
+    return "lol"
 @app.route("/products") 
 def product():
     return render_template("products.html")
 
 @app.route("/details") 
 def details():
-   # details = detailsdata()
-   # return render_template("details.html", data= details)
-    return "lol"
+    details = detailsdata()
+    return render_template("details.html", data= details)
+   # return "lol"
 
 #if __name__== "__main__":
  #   app.run(debug=True)
