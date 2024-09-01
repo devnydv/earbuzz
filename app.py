bbcd02ee15d09db00bcceec99b52f158c4d831c3
devnydv <bittumailb2@gmail.com>
2024-09-01 23:49:37 +0530

    some changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import json
-
+from db import inddata, catdata
 
 app = Flask(__name__)
 f = open("amazon.json", "r")
@@ -8,20 +8,27 @@ d = open("det.json", "r", encoding='utf-8' )
 
 #print(f.read())
 
-data = json.load(f)
-data = data['results']
+
 details = json.load(d)
 pdetails = details['productDetails']
 
 
 @app.route("/")
 def index():
-    #data = senddata()
+
+    data = inddata()
     return render_template("index.html", data= data)
 
-@app.route("/products") 
-def product():
-    return render_template("products.html")
+@app.route("/<cat>/") 
+def product(cat):
+    
+    catd = catdata(cat)
+    
+    if catd != []:
+        return render_template("products.html", data = catd )
+    else:
+        return redirect(url_for('error_404'))
+        
 
 @app.route("/details") 
 def detail():
@@ -32,6 +39,17 @@ def detail():
 def newdetail():
     #details = detailsdata()
     return render_template("detailsnew.html", data= pdetails, alld = details)
+
+# Error handling
+
+
+
+
+# Custom error pages
+@app.route('/error_404')
+def error_404():
+    return render_template("error.html") , 404
+    #return "This is a custom 404 error page.", 404
 
 
 if __name__== "__main__":
---
+++ db.py
@@ -0,0 +1,17 @@
+from pymongo.mongo_client import MongoClient
+import requests
+api = "mongodb+srv://bittumail:12356789@cluster0.fqrswkj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(api)
+db = client.earbuzz
+collection = db.item
+detailsdb = db.details
+
+def inddata():
+    home = list(collection.find().limit(16))
+    return home
+
+def catdata(cat):
+    filtered = list(collection.find({"cat": cat}))
+    return filtered
+
